9e53d2eeb6e37f6ac2da1dfa21139168575df4b5
Doai Tran <josdoaitran@gmail.com>
2022-03-31 08:44:46 +0700

    Update health_check.py

--- health_check.py
+++ health_check.py
@@ -6,3 +6,9 @@ app = Flask(__name__)
 @app.route('/')
 def health_check():
     return 'health_check. OK'
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
+
+# Why you should run it at 0.0.0.0
+# https://stackoverflow.com/questions/30323224/deploying-a-minimal-flask-app-in-docker-server-connection-issues
