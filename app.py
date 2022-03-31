d2d2c26b9afe6f1a93fd4d2e647f19f90fe96d31
Doai Tran <josdoaitran@gmail.com>
2022-03-31 15:30:24 +0700

    Update and rename health_check.py to app.py

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 from flask import Flask
-
 app = Flask(__name__)
 
-
-@app.route('/')
-def health_check():
-    return 'health_check. OK'
+@app.route('/check', methods=['GET'])
+def hello():
+    return "checked!!!!"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(debug=True, port=8080, host='0.0.0.0')
 
 # Why you should run it at 0.0.0.0
 # https://stackoverflow.com/questions/30323224/deploying-a-minimal-flask-app-in-docker-server-connection-issues
